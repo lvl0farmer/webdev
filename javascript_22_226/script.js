041682cc5382ef9ebf6044404dcf3927c459e94f
rounddumpling <treytruong27@gmail.com>
2021-01-24 14:13:32 -0500

    javascript 22_222 continue on 01-24-2021

--- javascript_22_226/script.js
+++ javascript_22_226/script.js
@@ -20,3 +20,28 @@ const newMovies = movies.map(movie => (
 // OR 
 const newMovies = movies.map(movie => `${movies.title} - ${movies.rating/10}`);
 // --------------------
+function validUserNames(userArr) {
+    return userArr.filter(name => name.length < 10)
+}
+// --------------------
+const goodMovies = movies.filter(m => m.score > 80)
+const goodMovies = movies.map(m => m.title)			// we just want the title 
+// OR
+const goodMovies = movies.filter(m => m.score > 80).map(m => m.title);
+// OR
+const goodMovies = movies
+					.filter(m => m.score > 80)
+					.map(m => m.title);
+					const number = [1,2,3,4,5,6]
+// --------------------
+number.filter(n => {
+	return n === 4
+}) 
+
+const fixedNumber = number.filter(n => {
+	return n === 4
+}) 
+// OR 
+const fixedNumber = number.filter(n => {
+	return n === 4
+}) 
