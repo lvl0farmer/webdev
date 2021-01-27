0339b20601bca634a98b8657d925114fc3da5498
rounddumpling <treytruong27@gmail.com>
2021-01-27 03:07:13 -0500

    javascript 23 227

--- javascript_23_227/script.js
+++ javascript_23_227/script.js
@@ -14,3 +14,23 @@ function rollDie(numSides = 6) {
 function greet(person, msg="Hey there", punc = '!'){			//it's recommended to leave the default parameter last 
 	console.log(`${msg}, ${person}${punc}`)
 }
+// ---------------------------------------------------
+
+// spread 			//expanding
+// we can expand argument/array 
+// ie: 
+Math.max(1,2,3,4,5,6,7,8,9,55,00,799,221,79,33) 			//it doesn't matter 
+Math.min(2,5,1)
+
+// so if 
+const nums = [1,2,3,4,5,6,7,8,9,55,00,799,221,79,33]
+Math.max(nums)													//this will print back an array 
+
+// is the same as 
+
+Math.max(...nums)												//this will print the same but actual value seperate by space
+
+
+// ALSO 
+console.log('hello')											// will print hello 
+console.log('...hello')											// will print h e l l o 
