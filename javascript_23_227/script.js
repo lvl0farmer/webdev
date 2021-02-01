d01e7b4ad7a2e5cb46c3270c842ea9d4e6815e83
rounddumpling <treytruong27@gmail.com>
2021-02-01 01:18:51 -0500

    javacript 22 237

--- javascript_23_227/script.js
+++ javascript_23_227/script.js
@@ -34,3 +34,54 @@ Math.max(...nums)												//this will print the same but actual value seperat
 // ALSO 
 console.log('hello')											// will print hello 
 console.log('...hello')											// will print h e l l o 
+// ---------------------------------------------------
+
+//spread object literal 
+const feline = {leges: 4, family: 'Felidae'};
+const canine = {family: 'Cainae', furry: true};
+
+
+const dog = {...canine, isPet: true}; 
+// same property, the last one will be set 
+const doggie = {...canine, ...feline}; 						     // this will be {leges: 4, family: 'Cainae', furry: true}
+// ---------------------------------------------------
+const dataFromForm = {
+	email: 'something@gmail.com', 
+	password: 'soemthing', 
+	username: 'something2'
+}
+
+const newUser = {...dataFromForm, id: 01, isAdmin: true}
+// ---------------------------------------------------
+// Rest Params 
+
+function print(){									//this would work 
+	console.log(arguments)
+}
+
+print(1,2,3,4) 											//1 2 3 4 
+
+// BUT this does not work, since arguments is technically not an array 
+function print(){	
+	return arguments.reduce((total, el) => total + el)	
+}
+
+// so we use this spread 
+function print(...num){	
+	return num.reduce((total, el) => total + el)	
+}
+
+// OR
+function raceResults(gold, silver, ...everyoneElse){
+	console.log(`Gold medal goes to: ${gold}`)
+	console.log(`Silver medal goes to: ${silver}`)
+	console.log(`Thanks for participating: ${everyoneElse}`)
+}
+
+raceResults('amy','tod', 'tina', 'trevor', 'travis')				//first parameter, second parameter, and everyone else
+
+//the builtin `arguments` object does not work in arrow function 
+//spread is collecting things into a single parameter 
+// ---------------------------------------------------
+
+
